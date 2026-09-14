10e57732da550dd512165974eafaba227f09357f
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-09-14 22:50:26 +0200

    Update: callback update &  error function

--- src/callbackVersion.ts
+++ src/callbackVersion.ts
@@ -1,11 +1,30 @@
-// notes from the callback hell part 1 videos
-// fetch userID
-// Fetch UserDetails
-// Save user log
-// display success 
-
 // sprint 2: Callback Version
 // - Implement fetching weather and news using https module with callback
 // - Demonstrate "callback hell" by nesting dependent calls
- 
-import http  from 'http';
+import "dotenv/config"
+import * as readline from "node:readline";
+import * as https from "node:https";
+
+// weather api call from .env
+const apiKey = process.env.OPENWEATHER_API_KEY;
+
+// if is not the api key then show this error 
+if (!apiKey) {
+    console.error("OPENWEATHER_API_KEY is not configured.");
+    process.exit(1);
+}
+
+const terminal = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+terminal.question("Which city are you in? ", (city) => {
+    const cityName = city.trim();
+
+    if (!cityName) {
+        console.error("Please enter a city name.");
+        terminal.close();
+        return;
+    }
+})
