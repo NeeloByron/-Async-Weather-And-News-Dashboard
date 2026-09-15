975e22f48c84cd59691303dee0be3657efa4b4b9
NeeloByron <byronnkhuna@gmail.com>
2026-09-15 09:30:18 +0200

    Create: AsyncAwaitVersion.ts file

---
+++ src/asyncAwaitVersion.ts
@@ -0,0 +1,4 @@
+// Sprint 4: AsyncAwaitVersion
+// Create asyncAwaitVersion.ts
+// - Refactor Promise code to use async/await
+// - Handle errors with try...catch
--- src/promiseVersion.ts
+++ src/promiseVersion.ts
@@ -1,2 +1,4 @@
 // Sprint 3: Promise Version
-// - 
+// - create a promiseVersion.ts
+// - Chain Promises to fetch weather -> news -> display
+// - Implement Promise.all() and Promise.race() examples
